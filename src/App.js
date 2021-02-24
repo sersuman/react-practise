e9c3aa913cba7bdb95b9c307b39b15fc5f3685a3
sersuman <sumanshrestha670@gmail.com>
2021-02-24 17:54:14 +0545

    controlled form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';  
-  
 class App extends React.Component {  
-   constructor(props){
-       super(props);
-       this.updateSubmit = this.updateSubmit.bind(this);
-       this.input = React.createRef();
-   }
-   updateSubmit(event){
-       alert('You have entered the UserName and CompanyName successfully.');
-       event.preventDefault();
-   }
-   render(){
-       return(
-           <form onSubmit={this.updateSubmit}>
-               <h1>Uncontrolled Form Example</h1>
-               <label>Name:
-                   <input type="text" ref={this.input} />
-               </label>
-               <label>Company Name:
-                   <input type="text" ref={this.input}/>
-               </label>
-               <input type="submit" value="Submit" />
-           </form>
-       )
-   }
+  constructor(props) {  
+      super(props);  
+      this.state = {value: ''};  
+      this.handleChange = this.handleChange.bind(this);  
+      this.handleSubmit = this.handleSubmit.bind(this);  
+  }  
+  handleChange(event) {  
+      this.setState({value: event.target.value});  
+  }  
+  handleSubmit(event) {  
+      alert('You have submitted the input successfully: ' + this.state.value);  
+      event.preventDefault();  
+  }  
+  render() {  
+      return (  
+          <form onSubmit={this.handleSubmit}>  
+            <h1>Controlled Form Example</h1>  
+            <label>  
+                Name:  
+                <input type="text" value={this.state.value} onChange={this.handleChange} />  
+            </label>  
+            <input type="submit" value="Submit" />  
+         </form>  
+      );  
+  }  
 }  
 export default App;  
