5eda63c6b3cb0ad1a387a0098893f7207d59315c
sersuman <sumanshrestha670@gmail.com>
2021-02-24 11:35:28 +0545

    Props and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+             name: "JavaPoint",
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <JTP jtpProp = {this.state.name}/>
+            </div>
+        );
+    }
+}
+class JTP extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    State & Props Example
+                </h1>
+                <h3> This is prop {this.props.jtpProp}</h3>
+                <p> JavaTPoint is not one of the best</p>
+            </div>
+        );
+    }
 }
 
 export default App;
