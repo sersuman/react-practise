09e9c88d7a9532ab9500b8ebbb1e55c5353fd8f4
sersuman <sumanshrestha670@gmail.com>
2021-02-24 15:05:38 +0545

    set state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';  
-  
-class App extends Component {  
-  constructor(props){  
-    super(props);  
-    this.state = {  
-         data: 'www.javatpoint.com'  
-      }  
-  }  
-   handleEvent = () => {  
-    console.log(this.props);  
-  }  
-  render() {  
-    return (  
-      <div className="App">  
-    <h2>React Constructor Example</h2>  
-    <input type ="text" value={this.state.data} />  
-        <button onClick={this.handleEvent}>Please Click</button>  
-      </div>  
-    );  
-  }  
-}  
-export default App; 
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            msg: "Welcome to Javapoint"
+        };
+        this.updateSetState = this.updateSetState.bind(this);
+
+    }
+
+    updateSetState(){
+        this.setState({
+            msg: "I am best react js "
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.msg}</h1>
+                <button onClick={this.updateSetState}>Set state</button>
+            </div>
+        );
+    }
+}
+
+export default App;
